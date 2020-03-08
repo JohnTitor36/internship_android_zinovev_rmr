0dd568ba7a3f78d663c70d963357d316198c373a
Ivan <izinovyev42@gmail.com>
2020-03-09 01:11:31 +0300

    Добавить LoginViewDataMapper

---
+++ features/login/src/main/java/com/lockwood/themoviedb/login/view/LoginViewDataMapper.kt
@@ -0,0 +1,31 @@
+package com.lockwood.themoviedb.login.view
+
+import com.lockwood.themoviedb.login.data.LoginModel
+
+object LoginViewDataMapper {
+
+    private const val MIN_PASSWORD_LENGTH = 4
+    private const val MIN_LOGIN_LENGTH = 4
+
+    fun loginToLoginViewData(model: LoginModel): LoginViewData {
+        val login = model.login
+        val password = model.password
+        val isValidateCredentials = validateCredentials(login, password)
+        return LoginViewData(
+            login = login,
+            password = password,
+            isValidInput = isValidateCredentials
+        )
+    }
+
+    private fun validateCredentials(login: CharSequence, password: String): Boolean {
+        val isValidLogin = validateLogin(login)
+        val isValidPassword = validatePassword(password)
+        return isValidLogin && isValidPassword
+    }
+
+    private fun validateLogin(login: CharSequence) = login.length > MIN_LOGIN_LENGTH
+
+    private fun validatePassword(pass: CharSequence) = pass.length > MIN_PASSWORD_LENGTH
+
+}
