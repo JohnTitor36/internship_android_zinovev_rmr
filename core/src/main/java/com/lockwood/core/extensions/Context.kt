64ffbcdeff654cff2c33d464f1406c3ef2bfd51c
Ivan <izinovyev42@gmail.com>
2020-03-09 01:29:23 +0300

    Добавить получение цвета и шрифта через Context extension

--- core/src/main/java/com/lockwood/core/extensions/Context.kt
+++ core/src/main/java/com/lockwood/core/extensions/Context.kt
@@ -3,6 +3,11 @@ package com.lockwood.core.extensions
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
+import androidx.annotation.ColorRes
+import androidx.annotation.FontRes
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 
 inline fun <reified T : Activity> Context.launchActivity(
     init: Intent.() -> Unit = {}
@@ -13,3 +18,7 @@ inline fun <reified T : Activity> Context.launchActivity(
 }
 
 inline fun <reified T : Any> newIntent(context: Context): Intent = Intent(context, T::class.java)
+
+fun Context.color(@ColorRes res: Int): Int = ContextCompat.getColor(this, res)
+
+fun Context.font(@FontRes res: Int): Typeface? = ResourcesCompat.getFont(this, res)
