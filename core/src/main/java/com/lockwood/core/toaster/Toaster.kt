2b76d4628a2cd9dcd35346d3233eea918c9767db
Ivan <izinovyev42@gmail.com>
2020-03-09 01:30:00 +0300

    Добавлен Toaster для показа тостов :)

---
+++ core/src/main/java/com/lockwood/core/toaster/Toaster.kt
@@ -0,0 +1,35 @@
+package com.lockwood.core.toaster
+
+import android.widget.Toast
+import com.lockwood.core.R
+
+interface Toaster {
+
+    companion object {
+
+        private val DEFAULT_BACKGROUND_TINT = R.color.primary
+        private val DEFAULT_TEXT_COLOR = R.color.primary
+    }
+
+    var currentToast: Toast?
+
+    fun toast(
+        str: String,
+        backgroundTint: Int = DEFAULT_BACKGROUND_TINT,
+        textColor: Int = DEFAULT_TEXT_COLOR
+    )
+
+    fun longToast(
+        str: String,
+        backgroundTint: Int = DEFAULT_BACKGROUND_TINT,
+        textColor: Int = DEFAULT_TEXT_COLOR
+    )
+
+    fun buildToast(
+        str: String,
+        backgroundTint: Int = DEFAULT_BACKGROUND_TINT,
+        textColor: Int = DEFAULT_TEXT_COLOR,
+        length: Int = Toast.LENGTH_SHORT
+    ): Toast
+
+}
