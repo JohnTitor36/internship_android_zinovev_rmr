e679cb0ad82513980bfcc83017919ed9f4040802
Ivan <izinovyev42@gmail.com>
2020-03-08 11:06:19 +0300

    Добавить переход из MainActivity в LoginActivity

--- app/src/main/java/com/lockwood/themoviedb/MainActivity.kt
+++ app/src/main/java/com/lockwood/themoviedb/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.lockwood.themoviedb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lockwood.themoviedb.extensions.launchActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launchActivity<LoginActivity>()
     }
 }
