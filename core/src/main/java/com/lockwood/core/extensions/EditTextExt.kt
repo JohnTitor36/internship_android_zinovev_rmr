001af18c7025e80ffb1e7cda54dc00cc92a9fc1b
Ivan <izinovyev42@gmail.com>
2020-03-09 23:21:06 +0300

    Добавить jvmTarget 1.8 в gradle файлы

---
+++ core/src/main/java/com/lockwood/core/extensions/EditTextExt.kt
@@ -0,0 +1,19 @@
+package com.lockwood.core.extensions
+
+import android.text.Editable
+import android.text.TextWatcher
+
+
+fun simpleTextWatcher(
+    textChanged: (String) -> Unit
+) = object :TextWatcher {
+
+    override fun afterTextChanged(s: Editable?) = Unit
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int)  = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+       textChanged(s.toString())
+    }
+
+}
--- features/login/src/main/java/com/lockwood/themoviedb/login/effecthandlers/LoginEffectHandlers.kt
+++ features/login/src/main/java/com/lockwood/themoviedb/login/effecthandlers/LoginEffectHandlers.kt
@@ -1,20 +1,32 @@
 package com.lockwood.themoviedb.login.effecthandlers
 
-import android.content.Context
 import com.lockwood.themoviedb.login.domain.LoginEffect
 import com.lockwood.themoviedb.login.domain.LoginEvent
+import com.lockwood.themoviedb.login.domain.NotifyLoginComplete
+import com.lockwood.themoviedb.login.domain.NotifyLoginInvalidInfo
 import com.lockwood.themoviedb.login.view.LoginViewActions
 
 import com.spotify.mobius.rx2.RxMobius
 import io.reactivex.ObservableTransformer
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 object LoginEffectHandlers {
 
     fun createEffectHandlers(
-        view: LoginViewActions,
-        context: Context
+        view: LoginViewActions
     ): ObservableTransformer<LoginEffect, LoginEvent> {
-        return RxMobius.subtypeEffectHandler<LoginEffect, LoginEvent>().build()
+        return RxMobius.subtypeEffectHandler<LoginEffect, LoginEvent>()
+            .addAction(
+                NotifyLoginComplete::class.java,
+                view::showLoginComplete,
+                AndroidSchedulers.mainThread()
+            )
+            .addAction(
+                NotifyLoginInvalidInfo::class.java,
+                view::showLoginInvalidInfo,
+                AndroidSchedulers.mainThread()
+            )
+            .build()
     }
 
 }
