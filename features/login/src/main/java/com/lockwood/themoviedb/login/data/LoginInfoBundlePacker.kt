1e0d619c9ae75f712d233314cd2d9a8340748731
Ivan <izinovyev42@gmail.com>
2020-03-09 23:34:16 +0300

    Добавить LoginInfoBundlePacker

---
+++ features/login/src/main/java/com/lockwood/themoviedb/login/data/LoginInfoBundlePacker.kt
@@ -0,0 +1,22 @@
+package com.lockwood.themoviedb.login.data
+
+import android.os.Bundle
+
+object LoginInfoBundlePacker {
+
+    private object LoginInfoIdentifiers {
+        const val LOGIN = "login_info_login"
+        const val PASSWORD = "login_info_password"
+    }
+
+    fun loginInfoToBundle(loginInfo: LoginInfo) = Bundle().apply {
+        putString(LoginInfoIdentifiers.LOGIN, loginInfo.login)
+        putString(LoginInfoIdentifiers.PASSWORD, loginInfo.password)
+    }
+
+    fun loginInfoFromBundle(bundle: Bundle) = LoginInfo(
+        login = bundle.getString(LoginInfoIdentifiers.LOGIN) ?: "",
+        password = bundle.getString(LoginInfoIdentifiers.PASSWORD) ?: ""
+    )
+
+}
