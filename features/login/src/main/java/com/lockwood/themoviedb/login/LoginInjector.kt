96b56bdf53bb18b7afafe4345e86c4a28c101fc5
Ivan <izinovyev42@gmail.com>
2020-03-09 20:34:34 +0300

    Добавить LoginInjector

---
+++ features/login/src/main/java/com/lockwood/themoviedb/login/LoginInjector.kt
@@ -0,0 +1,32 @@
+package com.lockwood.themoviedb.login
+
+import com.lockwood.themoviedb.login.data.LoginModel
+import com.lockwood.themoviedb.login.domain.LoginEffect
+import com.lockwood.themoviedb.login.domain.LoginEvent
+import com.lockwood.themoviedb.login.domain.LoginUpdate
+import com.spotify.mobius.MobiusLoop
+import com.spotify.mobius.MobiusLoop.Controller
+import com.spotify.mobius.android.AndroidLogger
+import com.spotify.mobius.android.MobiusAndroid
+import com.spotify.mobius.rx2.RxMobius
+import io.reactivex.ObservableTransformer
+
+object LoginInjector {
+
+    private const val TAG = "Login"
+
+    fun createController(
+        effectHandlers: ObservableTransformer<LoginEffect, LoginEvent>, defaultModel: LoginModel
+    ): Controller<LoginModel, LoginEvent> {
+        return MobiusAndroid.controller(createLoop(effectHandlers), defaultModel)
+    }
+
+    private fun createLoop(
+        effectHandlers: ObservableTransformer<LoginEffect, LoginEvent>
+    ): MobiusLoop.Factory<LoginModel, LoginEvent, LoginEffect> {
+        return RxMobius
+            .loop(LoginUpdate(), effectHandlers)
+            .logger(AndroidLogger.tag(TAG))
+    }
+
+}
