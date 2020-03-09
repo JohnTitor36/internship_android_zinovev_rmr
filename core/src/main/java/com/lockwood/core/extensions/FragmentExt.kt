1caa91be252687d2c80175be39be2f4797e7dfc3
Ivan <izinovyev42@gmail.com>
2020-03-10 00:17:49 +0300

    Добавить hideKeyboard в FragmentExt

--- core/src/main/java/com/lockwood/core/extensions/FragmentExt.kt
+++ core/src/main/java/com/lockwood/core/extensions/FragmentExt.kt
@@ -2,10 +2,22 @@ package com.lockwood.core.extensions
 
 import android.content.Context
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 val Fragment.ctx: Context
     get() = requireContext()
+
+fun Fragment.hideKeyboard() {
+    val view = view?.rootView
+    view?.let {
+        val imm =
+            context?.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(it.windowToken, 0)
+        it.clearFocus()
+    }
+}
 
 inline fun <reified T : Fragment> newFragment(
     init: Bundle.() -> Unit = {}
