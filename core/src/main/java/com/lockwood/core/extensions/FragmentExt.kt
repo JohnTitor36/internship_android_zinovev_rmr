33bb06f76183927e0ec7e2181e7fae07f5f60292
Ivan <izinovyev42@gmail.com>
2020-03-09 19:50:11 +0300

    Переименовать newInstance в newFragment

--- core/src/main/java/com/lockwood/core/extensions/FragmentExt.kt
+++ core/src/main/java/com/lockwood/core/extensions/FragmentExt.kt
@@ -7,7 +7,7 @@ import androidx.fragment.app.Fragment
 val Fragment.ctx: Context
     get() = requireContext()
 
-inline fun <reified T : Fragment> newInstance(
+inline fun <reified T : Fragment> newFragment(
     init: Bundle.() -> Unit = {}
 ): T {
     val fragment = T::class.java.newInstance()
