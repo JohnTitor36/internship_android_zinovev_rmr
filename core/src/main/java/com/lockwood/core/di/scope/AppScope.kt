17e58015cd72b5a5b6751d13083aac8b80b4f0cb
Ivan <izinovyev42@gmail.com>
2020-03-12 14:59:10 +0300

    Убрать ненужные комментарии

--- core/src/main/java/com/lockwood/core/di/scope/AppScope.kt
+++ core/src/main/java/com/lockwood/core/di/scope/AppScope.kt
@@ -2,9 +2,6 @@ package com.lockwood.core.di.scope
 
 import javax.inject.Scope
 
-/**
- * Scope for a feature module.
- */
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
 annotation class AppScope
--- core/src/main/java/com/lockwood/core/di/scope/FeatureScope.kt
+++ core/src/main/java/com/lockwood/core/di/scope/FeatureScope.kt
@@ -2,9 +2,6 @@ package com.lockwood.core.di.scope
 
 import javax.inject.Scope
 
-/**
- * Scope for the entire app runtime.
- */
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
 annotation class FeatureScope
