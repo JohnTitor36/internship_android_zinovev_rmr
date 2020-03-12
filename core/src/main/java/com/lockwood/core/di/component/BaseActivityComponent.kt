273e20940176ebf166e29cdad4a0160c7b381ad2
Ivan <izinovyev42@gmail.com>
2020-03-12 14:59:51 +0300

    Вынести BaseActivityComponent

---
+++ core/src/main/java/com/lockwood/core/di/component/BaseActivityComponent.kt
@@ -0,0 +1,5 @@
+package com.lockwood.core.di.component
+
+import android.app.Activity
+
+interface BaseActivityComponent<T : Activity> : BaseComponent<T>
--- core/src/main/java/com/lockwood/core/di/component/BaseComponent.kt
+++ core/src/main/java/com/lockwood/core/di/component/BaseComponent.kt
@@ -1,10 +1,6 @@
 package com.lockwood.core.di.component
 
-import android.app.Activity
-
 interface BaseComponent<T> {
 
     fun inject(target: T)
 }
-
-interface BaseActivityComponent<T : Activity> : BaseComponent<T>
