2f16b0d73486a5f29ed19a5b1f5aa9bf59607652
Ivan <izinovyev42@gmail.com>
2020-03-13 15:54:41 +0300

    Убрать Toaster

--- features/login/src/main/java/com/lockwood/themoviedb/login/presentation/LoginActivity.kt
+++ features/login/src/main/java/com/lockwood/themoviedb/login/presentation/LoginActivity.kt
@@ -2,16 +2,11 @@ package com.lockwood.themoviedb.login.presentation
 
 import android.os.Bundle
 import com.lockwood.core.extensions.appToolsProvider
-import com.lockwood.core.toaster.Toaster
 import com.lockwood.core.ui.BaseActivity
 import com.lockwood.themoviedb.login.R
 import com.lockwood.themoviedb.login.di.component.DaggerLoginComponent
-import javax.inject.Inject
 
 class LoginActivity : BaseActivity(R.layout.activity_login) {
-
-    @Inject
-    lateinit var toaster: Toaster
 
     override fun onCreate(savedInstanceState: Bundle?) {
         inject()
