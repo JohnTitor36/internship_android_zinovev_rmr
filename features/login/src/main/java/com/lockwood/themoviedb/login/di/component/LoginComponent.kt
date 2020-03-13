60e8985feec925486215c6796f6889401d8a27b1
Ivan <izinovyev42@gmail.com>
2020-03-13 15:02:01 +0300

    Переименовать LoginActivityModule в LoginModule

--- features/login/src/main/java/com/lockwood/themoviedb/login/di/component/LoginComponent.kt
+++ features/login/src/main/java/com/lockwood/themoviedb/login/di/component/LoginComponent.kt
@@ -3,13 +3,13 @@ package com.lockwood.themoviedb.login.di.component
 import com.lockwood.core.di.component.BaseActivityComponent
 import com.lockwood.core.di.provider.ApplicationProvider
 import com.lockwood.core.di.scope.FeatureScope
-import com.lockwood.themoviedb.login.di.module.LoginActivityModule
+import com.lockwood.themoviedb.login.di.module.LoginModule
 import com.lockwood.themoviedb.login.presentation.LoginActivity
 import dagger.Component
 
 @Component(
     dependencies = [ApplicationProvider::class],
-    modules = [LoginActivityModule::class]
+    modules = [LoginModule::class]
 )
 @FeatureScope
 interface LoginComponent : BaseActivityComponent<LoginActivity> {
--- features/login/src/main/java/com/lockwood/themoviedb/login/di/module/LoginModule.kt
+++ features/login/src/main/java/com/lockwood/themoviedb/login/di/module/LoginModule.kt
@@ -8,7 +8,7 @@ import dagger.Module
 import dagger.Provides
 
 @Module
-class LoginActivityModule {
+class LoginModule {
 
     @Provides
     @FeatureScope
