df3ae6756ecefcea6b209a7df64c7f980b4a2104
Ivan <izinovyev42@gmail.com>
2020-03-14 17:59:20 +0300

    Обновить navigationBarColor

--- features/login/src/main/java/com/lockwood/themoviedb/login/presentation/ui/LoginActivity.kt
+++ features/login/src/main/java/com/lockwood/themoviedb/login/presentation/ui/LoginActivity.kt
@@ -1,6 +1,15 @@
 package com.lockwood.themoviedb.login.presentation.ui
 
+import android.os.Bundle
+import com.lockwood.core.extensions.color
 import com.lockwood.core.ui.BaseActivity
 import com.lockwood.themoviedb.login.R
 
-class LoginActivity : BaseActivity(R.layout.activity_login)
+class LoginActivity : BaseActivity(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        window.navigationBarColor = color(R.color.bg_black)
+        super.onCreate(savedInstanceState)
+    }
+
+}
