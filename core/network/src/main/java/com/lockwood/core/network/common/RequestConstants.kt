dfd5b4863a4991dbabd5190a444a6e775bce58c9
Ivan <izinovyev42@gmail.com>
2020-03-22 21:38:58 +0300

    Исправить RequestConstants нейминг

--- core/network/src/main/java/com/lockwood/core/network/common/RequestConstants.kt
+++ core/network/src/main/java/com/lockwood/core/network/common/RequestConstants.kt
@@ -2,8 +2,8 @@ package com.lockwood.core.network.common
 
 object RequestConstants {
 
-    const val CONTENT_TYPE_HEADER = "Content-Type"
+    const val HEADER_CONTENT_TYPE = "Content-Type"
 
-    const val HEADER_CONTENT_TYPE_JSON = "application/json"
+    const val CONTENT_TYPE_JSON = "application/json"
 
 }
--- core/network/src/main/java/com/lockwood/core/network/interceptor/OkHttpHeaderInterceptor.kt
+++ core/network/src/main/java/com/lockwood/core/network/interceptor/OkHttpHeaderInterceptor.kt
@@ -1,7 +1,7 @@
 package com.lockwood.core.network.interceptor
 
-import com.lockwood.core.network.common.RequestConstants.CONTENT_TYPE_HEADER
-import com.lockwood.core.network.common.RequestConstants.HEADER_CONTENT_TYPE_JSON
+import com.lockwood.core.network.common.RequestConstants.HEADER_CONTENT_TYPE
+import com.lockwood.core.network.common.RequestConstants.CONTENT_TYPE_JSON
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -10,7 +10,7 @@ class OkHttpHeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
-            .addHeader(CONTENT_TYPE_HEADER, HEADER_CONTENT_TYPE_JSON).build()
+            .addHeader(HEADER_CONTENT_TYPE, CONTENT_TYPE_JSON).build()
         return chain.proceed(request)
     }
 
