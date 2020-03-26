efef9ae21e0206a03f8bfd9b8f41d2f1a6d85741
Ivan <izinovyev42@gmail.com>
2020-03-27 02:22:19 +0300

    Обновить LoginSuccessDispatcher

--- features/login/src/test/java/com/lockwood/themoviedb/login/LoginSuccessDispatcher.kt
+++ features/login/src/test/java/com/lockwood/themoviedb/login/LoginSuccessDispatcher.kt
@@ -2,6 +2,8 @@ package com.lockwood.themoviedb.login
 
 import com.lockwood.test.extensions.notFoundResponse
 import com.lockwood.test.extensions.successResponse
+import com.lockwood.themoviedb.login.ResponseConstants.REQUEST_TOKEN
+import com.lockwood.themoviedb.login.ResponseConstants.SESSION_ID
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
@@ -16,13 +18,40 @@ internal class LoginSuccessDispatcher : Dispatcher() {
             "authentication/token/validate_with_login"
         private const val PATH_AUTHENTICATION_SESSION_NEW =
             "authentication/session/new"
+
+        private const val RESPONSE_AUTHENTICATION_NEW_TOKEN =
+            "{\n" +
+                    "  \"success\": true,\n" +
+                    "  \"expires_at\": \"2016-08-26 17:04:39 UTC\",\n" +
+                    "  \"request_token\": \"$REQUEST_TOKEN\"\n" +
+                    "}"
+        private const val RESPONSE_AUTHENTICATION_TOKEN_VALIDATE_WITH_LOGIN =
+            "{\n" +
+                    "  \"success\": true,\n" +
+                    "  \"expires_at\": \"2018-07-24 04:10:26 UTC\",\n" +
+                    "  \"request_token\": \"$REQUEST_TOKEN\"\n" +
+                    "}"
+        private const val RESPONSE_AUTHENTICATION_SESSION_NEW =
+            "{\n" +
+                    "  \"success\": true,\n" +
+                    "  \"session_id\": \"$SESSION_ID\"\n" +
+                    "}"
     }
 
     override fun dispatch(request: RecordedRequest): MockResponse {
         return when (request.path) {
-            PATH_AUTHENTICATION_NEW_TOKEN -> MockResponse().successResponse()
-            PATH_AUTHENTICATION_TOKEN_VALIDATE_WITH_LOGIN -> MockResponse().successResponse()
-            PATH_AUTHENTICATION_SESSION_NEW -> MockResponse().successResponse()
+            PATH_AUTHENTICATION_NEW_TOKEN -> {
+                MockResponse().successResponse()
+                    .setBody(RESPONSE_AUTHENTICATION_NEW_TOKEN)
+            }
+            PATH_AUTHENTICATION_TOKEN_VALIDATE_WITH_LOGIN -> {
+                MockResponse().successResponse()
+                    .setBody(RESPONSE_AUTHENTICATION_TOKEN_VALIDATE_WITH_LOGIN)
+            }
+            PATH_AUTHENTICATION_SESSION_NEW -> {
+                MockResponse().successResponse()
+                    .setBody(RESPONSE_AUTHENTICATION_SESSION_NEW)
+            }
             else -> MockResponse().notFoundResponse()
         }
     }
