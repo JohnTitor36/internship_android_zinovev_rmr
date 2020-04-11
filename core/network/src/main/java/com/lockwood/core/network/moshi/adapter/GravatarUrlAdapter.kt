3a334d28c39cb35b367da12c165b8e6db983fc2d
Ivan <izinovyev42@gmail.com>
2020-04-11 17:40:01 +0300

    Использовать REGEX в gravatarUrlToHash

--- core/network/src/main/java/com/lockwood/core/network/moshi/adapter/GravatarUrlAdapter.kt
+++ core/network/src/main/java/com/lockwood/core/network/moshi/adapter/GravatarUrlAdapter.kt
@@ -5,7 +5,6 @@ import com.lockwood.core.network.utils.image.GravatarUtils
 import com.lockwood.core.window.WindowManager
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
--- core/network/src/main/java/com/lockwood/core/network/utils/image/GravatarUtils.kt
+++ core/network/src/main/java/com/lockwood/core/network/utils/image/GravatarUtils.kt
@@ -12,19 +12,22 @@ class GravatarUtils @Inject constructor(
     private val resourceReader: ResourceReader
 ) {
 
+    companion object {
+
+        private const val GRAVATAR_HASH_REGEX = "(?<=avatar/)(.*)(?=\\?)"
+    }
+
     // https://www.gravatar.com/avatar/8uO0gUM8aNqYLs1OsTBQiXu0fEv?s=500
     fun hashToGravatarUrl(hash: String, @Px size: Int): String {
-        val imageUrl =  resourceReader.string(R.string.gravatar_url)
-        return imageUrl.format(hash,size)
+        val imageUrl = resourceReader.string(R.string.gravatar_url)
+        return imageUrl.format(hash, size)
     }
 
     // 8uO0gUM8aNqYLs1OsTBQiXu0fEv
     fun gravatarUrlToHash(url: String): String {
-//        val avatar = "/avatar/"
-//        val startIndex = url.indexOf(avatar) + avatar.length
-//        val endIndex = url.indexOf(GRAVATAR_IMAGE_SIZE) - 1
-//        return url.substring(startIndex, endIndex)
-        return url
+        val regex = GRAVATAR_HASH_REGEX.toRegex()
+        val hashMatchResult = requireNotNull(regex.find(url))
+        return hashMatchResult.value
     }
 
 }
